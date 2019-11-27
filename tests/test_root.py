17147b4a84de4be27b760738fee596e52d4d7e6c
James Addison <james@reciperadar.com>
2019-11-27 11:20:54 +0000

    Add root request test coverage

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,6 +1,8 @@
+from mock import patch
 import pytest
 
 from web.app import parse_description_ingreedypy
+
 
 def parser_tests():
     return {
@@ -16,10 +18,28 @@ def parser_tests():
         },
     }
 
+
 @pytest.mark.parametrize('description, expected', parser_tests().items())
-def test_request(description, expected):
+def test_parse_description_ingreedypy(description, expected):
     expected.update({'input': description, 'parser': 'ingreedypy'})
 
     result = parse_description_ingreedypy(description)
 
     assert result == expected
+
+
+def nyt_parser_stub(descriptions):
+    return [{'input': description} for description in descriptions]
+
+
+@patch('web.app.parse_descriptions_nyt', nyt_parser_stub)
+def test_request(client):
+    response = client.post('/', data={'descriptions[]': ['tomato']})
+
+    assert response.json == [{
+        'description': 'tomato',
+        'product': {
+            'product': 'tomato',
+            'product_parser': 'ingreedypy'
+        }
+    }]
