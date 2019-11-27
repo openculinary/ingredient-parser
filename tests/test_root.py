846515a5aa324785bb5f7d911ea657a7a217760b
James Addison <james@reciperadar.com>
2019-11-27 11:00:40 +0000

    Add ingreedypy parser test coverage

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,2 +1,25 @@
-def test_request(client):
-    client.post('/')
+import pytest
+
+from web.app import parse_description_ingreedypy
+
+def parser_tests():
+    return {
+        'tomato': {
+            'product': 'tomato',
+            'quantity': None,
+            'units': None
+        },
+        '1 kilogram beef': {
+            'product': 'beef',
+            'quantity': 1,
+            'units': 'kilogram'
+        },
+    }
+
+@pytest.mark.parametrize('description, expected', parser_tests().items())
+def test_request(description, expected):
+    expected.update({'input': description, 'parser': 'ingreedypy'})
+
+    result = parse_description_ingreedypy(description)
+
+    assert result == expected
