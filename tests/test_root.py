01af4bce85128a068b34e43853e4e64af657deeb
James Addison <james@reciperadar.com>
2019-12-03 22:01:48 +0000

    Refactor ingredient-parser test suite

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,77 +1,55 @@
 from mock import patch
+import pytest
+
+
+def request_tests():
+    return {
+        '100ml red wine': {
+            'product': 'red wine',
+            'quantity': 100,
+            'units': 'ml',
+        },
+        '1000 grams potatoes': {
+            'product': 'potatoes',
+            'units': 'g',
+            'quantity': 1000,
+        },
+        '2lb 4oz potatoes': {
+            'product': 'potatoes',
+            'quantity': 907.18,
+            'units': 'g',
+        },
+        'pinch salt': {
+            'product': 'salt',
+            'units': 'ml',
+            'quantity': 0.25,
+        },
+    }.items()
 
 
 def nyt_parser_stub(descriptions):
     return [{'input': description} for description in descriptions]
 
 
+@pytest.mark.parametrize('description,expected', request_tests())
 @patch('web.app.parse_descriptions_nyt', nyt_parser_stub)
-def test_request(client):
-    response = client.post('/', data={
-        'descriptions[]': [
-            '100ml red wine',
-            '1000 grams potatoes',
-            '2lb 4oz potatoes',
-            'pinch salt',
-        ]
-    })
+def test_request(client, description, expected):
+    response = client.post('/', data={'descriptions[]': description})
+    ingredient = response.json[0]
 
-    assert response.json == [{
-        'description': '100ml red wine',
-        'product': {
-            'product': 'red wine',
-            'product_parser': 'ingreedypy',
-        },
-        'units': 'ml',
-        'units_parser': 'ingreedypy+pint',
-        'quantity': 100,
-        'quantity_parser': 'ingreedypy+pint'
-    }, {
-        'description': '1000 grams potatoes',
-        'product': {
-            'product': 'potatoes',
-            'product_parser': 'ingreedypy',
-        },
-        'units': 'g',
-        'units_parser': 'ingreedypy+pint',
-        'quantity': 1000,
-        'quantity_parser': 'ingreedypy+pint'
-    }, {
-        'description': '2lb 4oz potatoes',
-        'product': {
-            'product': 'potatoes',
-            'product_parser': 'ingreedypy',
-        },
-        'units': 'g',
-        'units_parser': 'ingreedypy+pint',
-        'quantity': 907.18,
-        'quantity_parser': 'ingreedypy+pint'
-    }, {
-        'description': 'pinch salt',
-        'product': {
-            'product': 'salt',
-            'product_parser': 'ingreedypy',
-        },
-        'units': 'ml',
-        'units_parser': 'ingreedypy+pint',
-        'quantity': 0.25,
-        'quantity_parser': 'ingreedypy+pint'
-    }]
+    assert ingredient['product']['product'] == expected['product']
+    assert ingredient['quantity'] == expected['quantity']
+    assert ingredient['units'] == expected['units']
 
 
 @patch('web.app.parse_descriptions_nyt', nyt_parser_stub)
 def test_request_dimensionless(client):
     response = client.post('/', data={'descriptions[]': ['1 potato']})
+    ingredient = response.json[0]
 
-    assert response.json == [{
-        'description': '1 potato',
-        'product': {
-            'product': 'potato',
-            'product_parser': 'ingreedypy',
-        },
-        'quantity': 1,
-        'quantity_parser': 'ingreedypy+pint'
-    }]
+    assert ingredient['product']['product'] == 'potato'
+    assert ingredient['quantity'] == 1
+    assert 'units' not in ingredient
 
 
 @patch('web.app.parse_units')
@@ -80,56 +58,7 @@ def test_request_unit_parse_failure(parse_units, client):
     parse_units.return_value = None
 
     response = client.post('/', data={'descriptions[]': ['100ml red wine']})
+    ingredient = response.json[0]
 
-    assert response.json == [{
-        'description': '100ml red wine',
-        'product': {
-            'product': 'red wine',
-            'product_parser': 'ingreedypy',
-        },
-        'units': 'milliliter',
-        'units_parser': 'ingreedypy',
-        'quantity': 100,
-        'quantity_parser': 'ingreedypy'
-    }]
-
-
-@patch('web.app.parse_descriptions_nyt', nyt_parser_stub)
-def test_parser_fallbacks(client):
-    response = client.post('/', data={'descriptions[]': [
-        '500g/1lb potatoes',
-        '1lb/500g potatoes',
-        '/500g potatoes',
-    ]})
-
-    assert response.json == [{
-        'description': '500g/1lb potatoes',
-        'product': {
-            'product': 'potatoes',
-            'product_parser': 'ingreedypy',
-        },
-        'quantity': 500,
-        'quantity_parser': 'ingreedypy+pint',
-        'units': 'g',
-        'units_parser': 'ingreedypy+pint'
-    }, {
-        'description': '1lb/500g potatoes',
-        'product': {
-            'product': 'potatoes',
-            'product_parser': 'ingreedypy',
-        },
-        'quantity': 453.59,
-        'quantity_parser': 'ingreedypy+pint',
-        'units': 'g',
-        'units_parser': 'ingreedypy+pint'
-    }, {
-        'description': '/500g potatoes',
-        'product': {
-            'product': 'potatoes',
-            'product_parser': 'ingreedypy',
-        },
-        'quantity': 500,
-        'quantity_parser': 'ingreedypy+pint',
-        'units': 'g',
-        'units_parser': 'ingreedypy+pint'
-    }]
+    assert 'pint' not in ingredient['quantity_parser']
+    assert 'pint' not in ingredient['units_parser']
