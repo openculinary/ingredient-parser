b76409dbba8d247c859e23f891e585ed20bda5de
James Addison <james@reciperadar.com>
2020-10-14 17:36:21 +0100

    Refactor density calculation code (#26)

--- web/app.py
+++ web/app.py
@@ -87,21 +87,21 @@ def parse_description(description):
     }
 
 
-def determine_density_ratio(product):
-    ratio = 1.0
+def determine_relative_density(ingredient):
+    product = ingredient['product']['product']
     if 'flour' in product:
-        ratio = 0.593
+        return 0.593
     elif 'sugar' in product:
-        ratio = 0.850
+        return 0.850
     elif 'milk' in product:
-        ratio = 1.030
+        return 1.030
     elif 'cream' in product:
-        ratio = 1.010
+        return 1.010
     elif 'oil' in product:
-        ratio = 0.900
+        return 0.900
     elif 'butter' in product:
-        ratio = 0.911
-    return ratio
+        return 0.911
+    return 1.0
 
 
 def determine_nutritional_content(ingredient):
@@ -117,8 +117,8 @@ def determine_nutritional_content(ingredient):
         grams = ingredient['magnitude']
     elif ingredient['units'] == 'ml':
         # convert to grams based on density
-        ratio = determine_density_ratio(ingredient['product']['product'])
-        grams = ingredient['magnitude'] * ratio
+        relative_density = determine_relative_density(ingredient)
+        grams = ingredient['magnitude'] * relative_density
     else:
         raise Exception(f"Unknown unit type: {ingredient['units']}")
 
